9c0e2aebfa60c1321879aa3edfdf251ac8d32c3c
Hao Wang <ge_nah@hotmail.com>
2019-06-06 22:25:13 -0700

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,14 @@
+class Appointment
+  attr_accessor :date, :doctor, :patient
+  @@all = []
+  def initialize(doctor, patient, date)
+    @doctor = doctor
+    @patient = patient
+    @date = date
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
